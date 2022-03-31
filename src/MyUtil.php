59af712d0a865c31c708fbe17040a960330673b6
xiongwenhui <xiongwh@mail.open.com.cn>
2022-03-31 15:29:48 +0800

    init

---
+++ src/MyUtil.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: open
+ * Date: 2022/3/31/14:42
+ */
+
+namespace Xiong\Say;
+
+
+class MyUtil
+{
+    public static function say($hello = 'hello'){
+        echo "you say :".$hello;
+    }
+}
---
+++ test/Test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * User: open
+ * Date: 2022/3/31/14:45
+ */
+require_once __DIR__.'/../vendor/autoload.php';
+
+\Xiong\Say\MyUtil::say('hahha');
+echo PHP_EOL;
